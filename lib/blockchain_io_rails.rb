334e686f91bfc2a0d788c886b715cd53b4d3647e
Fábio Araújo <fabioaraujo121@gmail.com>
2019-09-11 23:59:20 -0300

    Modularizing the api

--- lib/blockchain_io_rails.rb
+++ lib/blockchain_io_rails.rb
@@ -12,52 +12,7 @@ module BlockchainIoRails
        	end
     end
 
-  	module Wallet
-  		def create(args = {})
-  			uri = URI("http://localhost:3000/api/v2/create")
-  			pwd = encode_password(generate_password)
-            res = Net::HTTP.post_form(uri, 'password' => pwd)
-            if res.code == 200
-                puts res.body
-                {
-                    guid: res.body.guid,
-                    address: res.body.address,
-                    label: res.body.label,
-                    password: pwd
-                }
-            else
-                puts "Algo deu errado"
-                puts res.body
-                false
-            end
-  		end
-
-  		def outgoing(user, to, amount)
-  			satoshi = amount
-  			uri = URI("http://localhost:3000/merchant/" + user.guid + "/payment")
-            res = Net::HTTP.post_form(uri, 'password' => decode_password(user.wallet_password), 'to' => to, 'amount' => satoshi)
-            if res.code == 200
-                puts res.body
-                {
-                    message: res.body.message,
-                    tx_hash: res.body.tx_hash,
-                    notice: res.body.notice
-                }
-            else
-                puts "Algo deu errado"
-                puts res.body
-                false
-            end
-  		end
-
-  		def address_balance(user, address = nil)
-  			address ||= user.wallet
-  			uri = URI("http://localhost:3000/merchant/" + user.guid + "/address_balance?password=" + decode_password(user.wallet_password) + "&address=" + address)
-			Net::HTTP.get(uri)
-  		end
-  	end
-
-  	private
+  	protected
   		def encode_password(pwd)
   			crypt = ActiveSupport::MessageEncryptor.new(BlockchainIoRails.configuration.secret_key)
   			crypt.encrypt_and_sign(pwd)
@@ -70,3 +25,5 @@ module BlockchainIoRails
   			SecureRandom.hex(32)
   		end
 end
+
+require 'api/wallet/wallet'
