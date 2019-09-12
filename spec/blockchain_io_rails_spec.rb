777c16642bd0b759f7707e55e0c84766995fd0a8
Fábio Araújo <fabioaraujo121@gmail.com>
2019-09-11 23:59:47 -0300

    Implementing tests

--- spec/blockchain_io_rails_spec.rb
+++ spec/blockchain_io_rails_spec.rb
@@ -3,7 +3,27 @@ RSpec.describe BlockchainIoRails do
 		expect(BlockchainIoRails::VERSION).not_to be nil
 	end
 
-	it "does something useful" do
-		expect(false).to eq(true)
+	it "has api key" do
+		expect(BlockchainIoRails::configuration::api_key).not_to be nil
 	end
+
+	describe "Wallet API" do
+		it "create wallet" do
+			res = BlockchainIoRails::Api::Wallet::create
+			expect(res).to have_key(:guid)
+		end
+
+		it "outgoing payment" do
+			user = User.first
+			to = user.address
+			res = BlockchainIoRails::Api::Wallet::outgoing(user, to, 0.001)
+			expect(res).to have_key(:tx_hash)
+		end
+
+		it "address balance" do
+			user = User.first
+			res = BlockchainIoRails::Api::Wallet::address_balance(user)
+			expect(res).to be_kind_of(String)
+		end
+	end	
 end
